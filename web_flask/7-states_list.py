33b11a35d4d43113e98a5d9e8fbef539ab99e5d9
jesse-jagger <jessejackson.055@gmail.com>
2024-05-06 19:17:16 -0700

    updates

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """display the states and cities listed in alphabetical order"""
+    """displays the states and cities listed in alphabetical order"""
     states = storage.all("State").values()
-    return render_template('8-cities_by_states.html', states=states)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
